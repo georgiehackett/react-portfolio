0d7872258254d63098ce7fdd55f19972c634f2cc
Georgie <georgiehackett47@gmail.com>
2024-02-02 17:47:20 +0000

    Add router to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 
+function App() {
+  return (
+    <Router>
+      <Header />
+      {/* Wrap Route elements in a Routes component */}
+      <Routes>
+        {/* Define routes using the Route component to render different page components at different paths */}
+        {/* Define a default route that will render the Home component */}
+        <Route path="/" element={<Home />} />
+        <Route path="projects" element={<ProjectGallery />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
 
-
-export default App
+export default App;
